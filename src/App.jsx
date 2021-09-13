4604eae07366c803d6138628f0a4b5cfe45dc20c
Joe Dietrich <joe@joedietrich.net>
2021-09-12 20:45:40 -0400

    Done.

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import Signup from "./Signup";
+import Messages from "./Messages";
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/messages">
+          <Messages />
+        </Route>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Messages.jsx
@@ -0,0 +1,10 @@
+function Messages() {
+  return (
+    <div>
+      <h1>Read Your Messages</h1>
+      <p>I'm a message!</p>
+    </div>
+  );
+}
+
+export default Messages;
